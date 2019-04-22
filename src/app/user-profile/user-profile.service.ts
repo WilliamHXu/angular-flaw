03b1c6a1d817498c5a51f8aaffe39a654116e91a
laxmithoodi <laxmir.117@gmail.com>
2019-04-22 09:54:12 -0400

    Pussh From Intellij user changes

---
+++ src/app/user-profile/user-profile.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { User} from '../user';
+
+import { Observable } from 'rxjs/Observable';
+
+const httpOptions = {
+  headers: new HttpHeaders( { 'Content-Type': 'application/json'})
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserProfileService {
+
+  constructor(private  http: HttpClient) { }
+
+  private userUrl = 'http://localhost:8080/users';
+  // private userUrl = '/api';
+
+  // public getUsers() {
+  //   return this.http.get<{UsersList: User[]}>(this.userUrl);
+  // }
+
+  // getAll(): Observable<any> {
+  //   return this.http.get(this.userUrl);
+  // }
+
+  public getUsers() {
+    return this.http.get<User[]>(this.userUrl);
+  }
+  public deleteUser(user) {
+    return this.http.delete(this.userUrl + '/' + user.id);
+  }
+
+  public createUser(user) {
+    return this.http.post<User>(this.userUrl, user);
+  }
+
+}
