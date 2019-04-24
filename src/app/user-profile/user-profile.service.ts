0a899ea308516b48da83061356b9df5caee2a600
laxmithoodi <laxmir.117@gmail.com>
2019-04-24 13:38:20 -0400

    uer display on header

--- src/app/user-profile/user-profile.service.ts
+++ src/app/user-profile/user-profile.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { User} from '../user';
 
 import { Observable } from 'rxjs/Observable';
+import {Subject} from 'rxjs';
 
 const httpOptions = {
   headers: new HttpHeaders( { 'Content-Type': 'application/json'})
@@ -12,10 +13,13 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class UserProfileService {
+  public userName = new Subject<string>();
+  userChanged = this.userName.asObservable();
 
   constructor(private  http: HttpClient) { }
 
   private userUrl = 'http://localhost:8080/users';
+
   // private userUrl = '/api';
 
   // public getUsers() {
@@ -30,11 +34,24 @@ export class UserProfileService {
     return this.http.get<User[]>(this.userUrl);
   }
   public deleteUser(user) {
-    return this.http.delete(this.userUrl + '/' + user.id);
+    // console.log(user);
+    // console.log(user.id);
+
+    return this.http.delete(this.userUrl + '/' + user.id );
   }
 
   public createUser(user) {
     return this.http.post<User>(this.userUrl, user);
+  }
+
+  getUser(user) {
+    console.log(user);
+    // console.log(user.id);
+
+    return this.http.get(this.userUrl + '/' + user.id);
+  }
+  setUserName(userName: string) {
+    this.userName.next(userName);
   }
 
 }
